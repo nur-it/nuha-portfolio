560d93d496e8314efdbc51c61a82df057f652654
dev-bipro <roh17ra@gmail.com>
2025-04-12 14:52:31 -0500

    1st commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("script.js loaded");
+const headerOpenBtn = document.querySelector(".mobile_menu");
+const headerCloseBtn = document.querySelector(".mobile_nav_close_btn");
+const mobileNav = document.querySelector(".mobile_menu_overlay");
+
+headerOpenBtn.addEventListener("click", () => {
+  console.log("ok", mobileNav);
+  mobileNav.style.display = "block";
+});
+
+headerCloseBtn.addEventListener("click", () => {
+  console.log("ok", mobileNav);
+  mobileNav.style.display = "none";
+});
